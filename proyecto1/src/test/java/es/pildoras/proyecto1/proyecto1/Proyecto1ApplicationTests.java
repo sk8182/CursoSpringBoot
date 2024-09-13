7622df24edabb5c5ae7a12a9b551e7e5f41d6229
Julio Martinez <sk8182enkidu@gmail.com>
2024-09-13 11:33:41 +0200

    Curso Spring Boot. Spring Initializer. Starters. Vídeo 3 - crear nuestro primer proyecto con la herramienta Spring Initializer y el uso de los starters

---
+++ proyecto1/src/test/java/es/pildoras/proyecto1/proyecto1/Proyecto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.pildoras.proyecto1.proyecto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
