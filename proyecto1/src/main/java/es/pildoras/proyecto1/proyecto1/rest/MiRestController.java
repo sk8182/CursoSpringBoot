d1c35f9cfee110d5c48a01836c4dbce5c009deb3
Julio Martinez <sk8182enkidu@gmail.com>
2024-09-18 15:14:51 +0200

    Curso Spring Boot. DevTools. Video 8 -  instalamos la dependencia Spring Boot DevTools muy necesaria para el desarrollo de aplicaciones Spring Boot

--- proyecto1/src/main/java/es/pildoras/proyecto1/proyecto1/rest/MiRestController.java
+++ proyecto1/src/main/java/es/pildoras/proyecto1/proyecto1/rest/MiRestController.java
@@ -6,18 +6,26 @@ package es.pildoras.proyecto1.proyecto1.rest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author julio
  */
-@Controller//cambiamos a Controller
+@RestController//cambiamos a Controller
 public class MiRestController {
     
     @GetMapping("/") 
     public String saludo(){
         
         return "index";
+        
+    }
+    
+    @GetMapping("/info") 
+    public String informacion(){
+        
+        return "Esta es la web de los mejores programadores del mundo";
         
     }
     
